ed3a136ca1a967108e461fd52696a5f72db9c95c
poiu <75510171+notpoiu@users.noreply.github.com>
2024-02-10 00:31:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tiktok Love letter effect
+So recently ive been browsing tiktok when i found videos where people write a website love letter thing. I just found one of the videos and replicated
+the effect in html and css. I hope you like it. Made in under 1 hour, there might be some bugs for mobile. (Yes im that lazy to put min-height)
+
+[Original Design Inspiration](https://www.tiktok.com/@roizenx/video/7329961394260987141)
+
+# How to use
+Open `message.txt` and change the letter, you may use markdown formatting.
+
+# How to run
+To run locally, open `index.html` in your browser.
+However if you want to actually host this website, you can use github pages or use vercel.
+
+# License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*
+
+    ©️ 2023 upio
+
+    written by upio inspired by a tiktok video 
+    i was bored ok idek why im doing this 💀
+
+    https://www.upio.dev
+    https://e-z.bio/upio
+
+*/
+
+// uses markdownit
+function loadMessage(md) {
+    const message = document.querySelector("#message");
+
+    fetch("message.txt")
+        .then(response => response.text())
+        .then(data => {
+            message.innerHTML = md.render(data);
+        })
+        .catch(error => {
+            message.innerHTML = "An error occurred while fetching the message";
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let tempdata = {
+        "did_open": false
+    }
+
+    const heart = document.querySelector("#solid-heart");
+    const heart_container = document.querySelector(".heart-container");
+    
+    const msg_container = document.querySelector("#message-container");
+    
+    const md = window.markdownit({html: true});
+
+    loadMessage(md);
+
+    heart.addEventListener("click", function() {
+        if (tempdata.did_open) {
+            return;
+        }
+        tempdata.did_open = true;
+        
+        msg_container.style.display = "block";
+        heart_container.style.marginTop = "-1.5%";
+        
+        const auto_css_height = msg_container.scrollHeight + 20; // 20 extra padding
+        msg_container.style.height = auto_css_height + "px";
+    });
+
+});
